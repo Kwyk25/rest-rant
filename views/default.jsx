fca11e6939546ded675f530c866ba25f9ca7d075
Kyle <kylev006@gmail.com>
2023-04-20 21:04:17 -0700

    adding bootstrap grid to index page

--- views/default.jsx
+++ views/default.jsx
@@ -4,7 +4,9 @@ function Def (html) {
     return (
         <html>
             <head>
-                <title>Title</title>
+                <title>Title</title> 
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"></link>         
+                <link rel="stylesheet" href="/css/styles.css"/>
             </head>
             <body>
                 {html.children}
@@ -12,6 +14,5 @@ function Def (html) {
         </html>
     )
 }
-
 
 module.exports = Def
--- views/error404.jsx
+++ views/error404.jsx
@@ -1,16 +1,19 @@
-const React = require('react')
-const Def = require('./default')
+const React = require("react");
+const Def = require("./default");
 
-function error404 () {
-    return (
-      <Def>
-          <main>
-              <h1>404: PAGE NOT FOUND</h1>
-              <p>Oops, sorry, we can't find this page!</p>
-          </main>
-      </Def>
-    )
-  }
+function error404() {
+  return (
+    <Def>
+      <main>
+        <h1>404: PAGE NOT FOUND</h1>
+        <p>Oops, sorry, we can't find this page!</p>
+        <img
+          src="/images/404.jpg"
+          alt="whoops you have a 404 error better luck next time"
+        />
+      </main>
+    </Def>
+  );
+}
 
-
-module.exports = error404
+module.exports = error404;
